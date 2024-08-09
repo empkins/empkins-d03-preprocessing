2b8d5643e5f3f06620f335aee4a20b98b4d31287
Luca Abel <luca.abel@fau.de>
2024-08-09 12:58:18 +0200

    Video preprocessing finished

--- empkins_d03_preprocessing/video/video.py
+++ empkins_d03_preprocessing/video/video.py
@@ -1,30 +1,41 @@
 from pathlib import Path
 
 import pandas as pd
+from empkins_io.datasets.d03.micro_gapvii import MicroBaseDataset
 from moviepy.video.io.VideoFileClip import VideoFileClip
+from moviepy.editor import concatenate_videoclips
 
 
-def slice_video_to_phases(video_path: str, timelog_video: pd.DataFrame, output_path: str):
+def slice_video_to_phases(subset: MicroBaseDataset):
     """
     Slice video to the given start and end time and save it to the output path.
 
     Args:
-        video_path (Path): Path to the video file.
-        timelog_video (pd.DataFrame): Timelog DataFrame.
-        output_path (Path): Path to save the sliced video.
+        subset (MicroBaseDataset): Subset containing the video file and timelog.
     """
-    video = VideoFileClip(video_path)
+
+    if not subset.is_single(["subject", "condition", "phase"]):
+        raise ValueError("Subset must contain only one subject and one condition.")
+
+    video = VideoFileClip(str(subset.face_video_path))
+    timelog = subset.timelog_video
+    phase = subset.phase
+
+    if phase == "Talk":
+        talk_1 = video.subclip(timelog["start_talk_1"], timelog["start_pause_2"])
+        talk_2 = video.subclip(timelog["start_talk_2"], timelog["start_pause_3"])
+        sliced_video = concatenate_videoclips([talk_1, talk_2])
+    elif phase == "Math":
+        math_1 = video.subclip(timelog["start_math_1"], timelog["start_pause_4"])
+        math_2 = video.subclip(timelog["start_math_2"], timelog["start_pause_5"])
+        sliced_video = concatenate_videoclips([math_1, math_2])
+    else:
+        raise ValueError(f"Phase '{phase}' not in ['Talk', 'Math'].")
+
+    sliced_video.write_videofile(str(subset.face_video_path_sliced), audio_codec="aac", ffmpeg_params=["-c:v", "h264_videotoolbox"])
 
 
-    start_time = timelog_video["start_talk_1"]
-    end_time = timelog_video["start_pause_3"]
-    pause_start = timelog_video["start_pause_2"]
-    pause_end = timelog_video["start_talk_2"]
-
-    sliced_video = video.subclip(start_time, end_time).cutout(pause_start - start_time, pause_end - start_time)
-    sliced_video.write_videofile(output_path, codec="libx264", audio_codec="aac")
-
-def slice_video_conditions(video_path: str, timelog: pd.DataFrame, output_path: str):
+def slice_video_conditions(video_path: Path, timelog: pd.DataFrame, output_path: Path):
     """
     Slice video to the given start and end time and save it to the output path.
 
@@ -33,20 +44,19 @@ def slice_video_conditions(video_path: str, timelog: pd.DataFrame, output_path: 
         timelog (pd.DataFrame): Timelog DataFrame.
         output_path (Path): Path to save the sliced video.
     """
-    video = VideoFileClip(video_path)
 
-    start_time = timelog["start_talk_1"]
-    end_time = timelog["start_pause_5"]
-    timelog_relative = timelog - start_time
-    pause_2_start = timelog_relative["start_pause_2"]
-    pause_2_end = timelog_relative["start_talk_2"]
-    duration_pause_2 = pause_2_end - pause_2_start
-    pause_3_start = timelog_relative["start_pause_3"] - duration_pause_2
-    pause_3_end = timelog_relative["start_math_1"] - duration_pause_2
-    duration_pause_3 = pause_3_end - pause_3_start
-    pause_4_start = timelog_relative["start_pause_4"] - duration_pause_2 - duration_pause_3
-    pause_4_end = timelog_relative["start_math_2"] - duration_pause_2 - duration_pause_3
+    if not video_path.exists():
+        raise FileNotFoundError(f"Video file not found in {video_path}.")
 
-    sliced_video = video.subclip(start_time, end_time).cutout(pause_2_start, pause_2_end).cutout(pause_3_start, pause_3_end).cutout(pause_4_start, pause_4_end)
-    sliced_video.write_videofile(output_path, codec="libx264", audio_codec="aac")
+    video = VideoFileClip(str(video_path))
 
+    if timelog.drop("Infos").isna().any():
+        raise ValueError("Timelog contains NaN values.")
+
+    talk_1 = video.subclip(timelog["start_talk_1"], timelog["start_pause_2"])
+    talk_2 = video.subclip(timelog["start_talk_2"], timelog["start_pause_3"])
+    math_1 = video.subclip(timelog["start_math_1"], timelog["start_pause_4"])
+    math_2 = video.subclip(timelog["start_math_2"], timelog["start_pause_5"])
+
+    sliced_video = concatenate_videoclips([talk_1, talk_2, math_1, math_2])
+    sliced_video.write_videofile(str(output_path), audio_codec="aac", ffmpeg_params=["-c:v", "h264_videotoolbox"])
